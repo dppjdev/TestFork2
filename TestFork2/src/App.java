9a4aeed60b039b3a8ff4d55f92428335bc2a67c3
dppjdev2 <84398609+dppjdev2@users.noreply.github.com>
2021-07-01 17:14:23 -0700

    Update App.java
    added 1

--- TestFork2/src/App.java
+++ TestFork2/src/App.java
@@ -4,7 +4,7 @@ public class App {
 	// Added second line
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello1");
 	}
 
 }
